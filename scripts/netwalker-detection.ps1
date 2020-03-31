07c93a05b6993d719d2888f4c9c183c6551814d4
ahmetkotan <ahmtkotan@gmail.com>
2020-04-01 00:07:20 +0300

    added netwalker detection #1

---
+++ scripts/netwalker-detection.ps1
@@ -0,0 +1,25 @@
+$tmp_path = $env:TEMP
+$infected_processes = Get-Process | Where-Object {$_.Path -Like "*$tmp_path*"}
+
+Foreach ($process in $infected_processes) {
+    $description = $process.Description.ToLower()
+    $company = $process.Company.ToLower()
+    
+    IF ($description -Like "*wtv*" -And $company -Like "*microsoft*") {
+        Write-Host "Netwalker Detected."
+
+        $infected_pid = $process.Id
+        $infected_wmi = Get-WmiObject -Class Win32_Process | Where-Object {$_.ProcessId -Eq $infected_pid}
+        $executable_path = $infected_wmi.ExecutablePath
+        $infected_name = $infected_wmi.Name
+
+        $desktop_path = [Environment]::GetFolderPath("Desktop")
+        $destination_file_name = "$infected_name.malware"
+        $destination_path = Join-Path $desktop_path $destination_file_name
+
+        Stop-Process -Id $infected_pid -Force
+        Move-Item -Path $executable_path -Destination $destination_path
+    }
+
+}
+
